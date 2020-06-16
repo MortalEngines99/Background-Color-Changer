5bb5f099c7c2f1eb59eaa2836693faef413f0e55
Shane Sowdagar <strainedhalo1@gmail.com>
2020-06-16 13:51:14 +0400

    First Commit

---
+++ main.js
@@ -0,0 +1,29 @@
+const body = document.getElementsByTagName("body")[0];
+const changeBackgroundBtn = document.getElementById("changeBackground");
+const rgbParagraph = document.getElementById("rgbParagraph");
+let red,green,blue = 0;
+
+//Add spacebar event listener on body element
+body.addEventListener("keyup",(e) => {
+    if(e.code === "Space"){
+        changeBackgroundColor();
+    }
+});
+
+//Add click event listener on h3 element
+changeBackgroundBtn.addEventListener("click",() => changeBackgroundColor());
+
+function getRandomNumber(){
+    return Math.floor(Math.random() * Math.floor(256));
+}
+
+function changeBackgroundColor(){
+    let red = getRandomNumber();
+    let green = getRandomNumber();
+    let blue = getRandomNumber();
+
+    let rgb = `rgb(${red},${green},${blue})`;
+
+    body.style.backgroundColor = rgb;
+    rgbParagraph.innerText = rgb;
+}
