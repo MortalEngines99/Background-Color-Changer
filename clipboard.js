54ae0438aeaeb1346452ed6628f372f9e2787d17
Shane Sowdagar <strainedhalo1@gmail.com>
2020-06-16 15:47:27 +0400

    Add Alert for Successfully copying rgb color

--- clipboard.js
+++ clipboard.js
@@ -1,2 +1,30 @@
 // Instantiate Clipboard JS  
-new ClipboardJS('#rgbParagraph');
+let clipboard = new ClipboardJS('#rgbParagraph');
+const alert = document.getElementById("alert");
+
+clipboard.on('success', function(e) {
+
+    message = `${e.text} copied to clipboard`;
+
+    alert.classList.add("show");
+    alert.innerText = message;
+
+    setTimeout(() => {
+        alert.classList.remove("show");
+    }, 3000);
+
+
+    e.clearSelection();
+});
+
+clipboard.on('error', function(e) {
+    console.error('Action:', e.action);
+    console.error('Trigger:', e.trigger);
+
+    alert.classList.add("show");
+    alert.innerText = e.error;
+
+    setTimeout(() => {
+        alert.classList.remove("show");
+    }, 3000);
+});
